dd7eebdb3727f26b64e29bdffd9949ba50cd39bf
prince <rasealex000000@gmail.com>
2020-02-16 01:30:59 +0530

    fix

---
+++ app/config.py
@@ -0,0 +1,7 @@
+
+
+#----------Database MongoUri-----------
+#MongoUri ="mongodb://remoteuser:0vXPeLcPxME40Ydv@157.245.124.93/chainguardians?authSource=admin"
+#MongoUri ="mongodb+srv://root:0vXPeLcPxME40Ydv@cluster0-v8o7t.mongodb.net/chainguardians?retryWrites=true&w=majority"
+MongoUri ="mongodb+srv://xmage:xmage@cluster0-xooqb.mongodb.net/jobs_data?retryWrites=true"
+
