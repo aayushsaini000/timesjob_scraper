570c4b6b90f2500b379e4eed63230458a6d1197d
aayush <rasealex000000@gmail.com>
2020-02-19 13:23:03 +0530

    catg

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ mongo = db.init_db()
 
 #----------Import schedulers functions from files----------
 
-from app.chainguardians_scheduler import Timesjobs
+from app.chainguardians_scheduler import Timesjobs,accountjobs,HRjobs,Salesjobs,marketingjobs,customerjobs,legaljobs,Teachingjobs
 
 
 #----------created app functionality----------
@@ -41,11 +41,46 @@ def create_app(test_config=None):
 #-----------Date time Settings of chainguardians schedulers--------------
 
     Timesjobs_scheduler = BackgroundScheduler()
-    Timesjobs_scheduler.add_job(Timesjobs, trigger='cron', day_of_week='mon-sun', hour=00,minute=54)
-    #Timesjobs_scheduler.add_job(Timesjobs, trigger='interval', minutes =3000)#30
+    Timesjobs_scheduler.add_job(Timesjobs, trigger='cron', day_of_week='mon-sun', hour=13,minute=30)
     Timesjobs_scheduler.start()
+
+    accountjobs_scheduler = BackgroundScheduler()
+    accountjobs_scheduler.add_job(accountjobs, trigger='cron', day_of_week='mon-sun', hour=13,minute=30)
+    accountjobs_scheduler.start()
+
+    HRjobs_scheduler = BackgroundScheduler()
+    HRjobs_scheduler.add_job(HRjobs, trigger='cron', day_of_week='mon-sun', hour=13,minute=30)
+    HRjobs_scheduler.start()
+
+    Salesjobs_scheduler = BackgroundScheduler()
+    Salesjobs_scheduler.add_job(Salesjobs, trigger='cron', day_of_week='mon-sun', hour=13,minute=30)
+    Salesjobs_scheduler.start()
+
+    marketingjobs_scheduler = BackgroundScheduler()
+    marketingjobs_scheduler.add_job(marketingjobs, trigger='cron', day_of_week='mon-sun', hour=13,minute=30)
+    marketingjobs_scheduler.start()
+
+    customerjobs_scheduler = BackgroundScheduler()
+    customerjobs_scheduler.add_job(customerjobs, trigger='cron', day_of_week='mon-sun', hour=13,minute=30)
+    customerjobs_scheduler.start()
+
+    legaljobs_scheduler = BackgroundScheduler()
+    legaljobs_scheduler.add_job(legaljobs, trigger='cron', day_of_week='mon-sun', hour=13,minute=30)
+    legaljobs_scheduler.start()
+
+    Teachingjobs_scheduler = BackgroundScheduler()
+    Teachingjobs_scheduler.add_job(Teachingjobs, trigger='cron', day_of_week='mon-sun', hour=13,minute=30)
+    Teachingjobs_scheduler.start()
+
 
     try:
         return app
     except:   
         Timesjobs_scheduler.shutdown()
+        accountjobs_scheduler.shutdown()
+        HRjobs_scheduler.shutdown()
+        Salesjobs_scheduler.shutdown()
+        marketingjobs_scheduler.shutdown()
+        customerjobs_scheduler.shutdown()
+        legaljobs_scheduler.shutdown()
+        Teachingjobs_scheduler.shutdown()
--- app/chainguardians_scheduler.py
+++ app/chainguardians_scheduler.py
@@ -8,23 +8,169 @@ import datetime
 
 
 #--------Scheduler for fetching chainguardians assests details--------
+#it
+#account
+#HR Recruitment
+#Sales
+#marketing
+'''
+urls = ['https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=35$&txtKeywords=&pSize=1000&pIndex='+str(cou)+'',
+        'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=11$&txtKeywords=&pSize=1000&pIndex='+str(cou)+'',
+        'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=20$&txtKeywords=&pSize=1000&pIndex='+str(cou)+'',
+        'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=32$&txtKeywords=&pSize=1000&pIndex='+str(cou)+'',
+        'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=23$&txtKeywords=&pSize=1000&pIndex='+str(cou)+'',
+        'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=25$&txtKeywords=&pSize=1000&pIndex='+str(cou)+'',
+        'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=22$&txtKeywords=&pSize=1000&pIndex='+str(cou)+'',
+        'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=34$&txtKeywords=&pSize=1000&pIndex='+str(cou)+''
+        ]
+'''
+             #'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=22$&txtKeywords=&pSize=1000&pIndex='+str(cou)+'',
+            #'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=34$&txtKeywords=&pSize=1000&pIndex='+str(cou)+''
 
-def Timesjobs():    
-    print("running")
+
+def Timesjobs():
+    print("timesjobs")
+    #urls = ['https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=35$&txtKeywords=&pSize=1000&pIndex='+str(cou)+''
+    #    ]
+    #for url in urls:
     for cou in range(0,50):
-        print(cou)
-        url = 'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=&txtKeywords=&pSize=1000&pIndex='+str(cou)+''
-        #contract_url=url.replace("{{token_id}}",''+str(assetId)+'')              
-        assets = requests.get(url=url)
-        response = assets.json()      
+        working_url = 'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=35$&txtKeywords=&pSize=1000&pIndex='+str(cou)+''
+        #contract_url=url.replace("{{token_id}}",''+str(assetId)+'')
+        assets = requests.get(url=working_url)
+        response = assets.json()
         jobs = response['jobsList']
         for job in jobs:
             companyName = job['companyName']
             Location = job['Location']
             adId = job['adId'] 
-            checking = mongo.db.timesjobs_data.find_one({"companyName":companyName,"Location":Location,"adId":adId})
-            if checking is None:
-                ret = mongo.db.timesjobs_data.insert(dict(job))
-            else:
-                print("============================================================already exists=============================================")
+            job_object = dict(job)
+            job_object['catg_key'] = "software development"
+            checking = mongo.db.timesjobs_data.update({"companyName":companyName,"Location":Location,"adId":adId},{"$set":dict(job_object)},upsert=True)
+        print("============================================================already exists=============================================",cou)
 
+
+def accountjobs():
+    print("accountjobs")
+    for cou in range(0,50):
+        working_url = 'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=11$&txtKeywords=&pSize=1000&pIndex='+str(cou)+''
+        #contract_url=url.replace("{{token_id}}",''+str(assetId)+'')
+        assets = requests.get(url=working_url)
+        response = assets.json()
+        jobs = response['jobsList']
+        for job in jobs:
+            companyName = job['companyName']
+            Location = job['Location']
+            adId = job['adId'] 
+            job_object = dict(job)
+            job_object['catg_key'] = "accounts"
+            checking = mongo.db.timesjobs_data.update({"companyName":companyName,"Location":Location,"adId":adId},{"$set":dict(job_object)},upsert=True)
+        print("============================================================already exists=============================================",cou)
+
+
+def HRjobs():
+    print("HRjobs")
+    for cou in range(0,50):
+        working_url = 'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=20$&txtKeywords=&pSize=1000&pIndex='+str(cou)+''
+        #contract_url=url.replace("{{token_id}}",''+str(assetId)+'')
+        assets = requests.get(url=working_url)
+        response = assets.json()
+        jobs = response['jobsList']
+        for job in jobs:
+            companyName = job['companyName']
+            Location = job['Location']
+            adId = job['adId'] 
+            job_object = dict(job)
+            job_object['catg_key'] = "HR Recruitment"
+            checking = mongo.db.timesjobs_data.update({"companyName":companyName,"Location":Location,"adId":adId},{"$set":dict(job_object)},upsert=True)
+        print("============================================================already exists=============================================",cou)
+
+
+def Salesjobs():
+    print("Salesjobs")
+    for cou in range(0,50):
+        working_url = 'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=32$&txtKeywords=&pSize=1000&pIndex='+str(cou)+''
+        #contract_url=url.replace("{{token_id}}",''+str(assetId)+'')
+        assets = requests.get(url=working_url)
+        response = assets.json()
+        jobs = response['jobsList']
+        for job in jobs:
+            companyName = job['companyName']
+            Location = job['Location']
+            adId = job['adId'] 
+            job_object = dict(job)
+            job_object['catg_key'] = "Sales"
+            checking = mongo.db.timesjobs_data.update({"companyName":companyName,"Location":Location,"adId":adId},{"$set":dict(job_object)},upsert=True)
+        print("============================================================already exists=============================================",cou)
+
+
+
+def marketingjobs():
+    print("marketingjobs")
+    for cou in range(0,50):
+        working_url = 'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=23$&txtKeywords=&pSize=1000&pIndex='+str(cou)+''
+        #contract_url=url.replace("{{token_id}}",''+str(assetId)+'')
+        assets = requests.get(url=working_url)
+        response = assets.json()
+        jobs = response['jobsList']
+        for job in jobs:
+            companyName = job['companyName']
+            Location = job['Location']
+            adId = job['adId'] 
+            job_object = dict(job)
+            job_object['catg_key'] = "marketing"
+            checking = mongo.db.timesjobs_data.update({"companyName":companyName,"Location":Location,"adId":adId},{"$set":dict(job_object)},upsert=True)
+        print("============================================================already exists=============================================",cou)
+
+
+def customerjobs():
+    print("customerjobs")
+    for cou in range(0,50):
+        working_url = 'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=25$&txtKeywords=&pSize=1000&pIndex='+str(cou)+''
+        #contract_url=url.replace("{{token_id}}",''+str(assetId)+'')
+        assets = requests.get(url=working_url)
+        response = assets.json()
+        jobs = response['jobsList']
+        for job in jobs:
+            companyName = job['companyName']
+            Location = job['Location']
+            adId = job['adId'] 
+            job_object = dict(job)
+            job_object['catg_key'] = "customer service"
+            checking = mongo.db.timesjobs_data.update({"companyName":companyName,"Location":Location,"adId":adId},{"$set":dict(job_object)},upsert=True)
+        print("============================================================already exists=============================================",cou)
+
+
+def legaljobs():
+    print("legaljobs")
+    for cou in range(0,50):
+        working_url = 'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=22$&txtKeywords=&pSize=1000&pIndex='+str(cou)+''
+        #contract_url=url.replace("{{token_id}}",''+str(assetId)+'')
+        assets = requests.get(url=working_url)
+        response = assets.json()
+        jobs = response['jobsList']
+        for job in jobs:
+            companyName = job['companyName']
+            Location = job['Location']
+            adId = job['adId'] 
+            job_object = dict(job)
+            job_object['catg_key'] = "legal"
+            checking = mongo.db.timesjobs_data.update({"companyName":companyName,"Location":Location,"adId":adId},{"$set":dict(job_object)},upsert=True)
+        print("============================================================already exists=============================================",cou)
+
+
+def Teachingjobs():
+    print("Teaching")
+    for cou in range(0,50):
+        working_url = 'https://jobbuzz.timesjobs.com/jobbuzz/loadMoreJobs.json?companyIds=&locationnames=&aosValues=&sortby=I&from=filter&faids=34$&txtKeywords=&pSize=1000&pIndex='+str(cou)+''
+        #contract_url=url.replace("{{token_id}}",''+str(assetId)+'')
+        assets = requests.get(url=working_url)
+        response = assets.json()
+        jobs = response['jobsList']
+        for job in jobs:
+            companyName = job['companyName']
+            Location = job['Location']
+            adId = job['adId'] 
+            job_object = dict(job)
+            job_object['catg_key'] = "Teaching Education"
+            checking = mongo.db.timesjobs_data.update({"companyName":companyName,"Location":Location,"adId":adId},{"$set":dict(job_object)},upsert=True)
+        print("============================================================already exists=============================================",cou)
